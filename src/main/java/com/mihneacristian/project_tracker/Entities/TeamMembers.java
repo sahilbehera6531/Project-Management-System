6c9b1a654c496e28752b7783073ff023aba9d4ff
Sahil Behera <sahilbehera111333@gmail.com>
2026-02-21 21:59:55 +0530

    Initial commit - Project Management System backend built with Spring Boot and MySQL

---
+++ src/main/java/com/mihneacristian/project_tracker/Entities/TeamMembers.java
@@ -0,0 +1,93 @@
+package com.mihneacristian.project_tracker.Entities;
+
+import java.util.List;
+
+import com.mihneacristian.project_tracker.DTO.TeamMembersDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "team_members")
+public class TeamMembers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    Integer memberId;
+
+    @Column(name = "first_name")
+    String firstName;
+
+    @Column(name = "last_name")
+    String lastName;
+
+    @Column(name = "email_address")
+    String emailAddress;
+
+
+    @OneToMany(mappedBy = "teamMemberOfProject")
+    List<Project> projects;
+
+    @OneToMany(mappedBy = "teamMemberOfItem")
+    List<Item> items;
+
+    public TeamMembers() {
+    }
+
+    public TeamMembers(Integer teamMemberId) {
+    }
+
+    public TeamMembers(TeamMembersDTO teamMembersDTO) {
+        this.lastName = teamMembersDTO.teamMemberLastName;
+        this.firstName = teamMembersDTO.teamMemberFirstName;
+        this.emailAddress = teamMembersDTO.teamMemberEmailAddress;
+    }
+
+    public Integer getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Integer memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Team_Members{" +
+                "memberId=" + memberId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/mihneacristian/project_tracker/Entities/Type.java
@@ -0,0 +1,67 @@
+package com.mihneacristian.project_tracker.Entities;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "type")
+public class Type {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "type_id")
+    Integer typeId;
+
+    @Column(name = "name", nullable = false)
+    String name;
+
+    @OneToMany(mappedBy = "typeOfItem")
+    List<Item> items;
+
+    public Type() {
+    }
+
+    public Type(String typeOfItem) {
+
+        this.name = typeOfItem;
+    }
+
+    public Integer getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Integer typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    @Override
+    public String toString() {
+        return "Type{" +
+                "typeId=" + typeId +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
